7568580fbc28b1baa271569da993eff648114c94
jpwakugawa <jp_wakugawa@outlook.com>
2022-11-11 15:36:23 -0400

    add json to kafka dispatcher

--- apps/kafka_dispatcher.py
+++ apps/kafka_dispatcher.py
@@ -1,18 +1,26 @@
 from utils.custom_logging import CustomLogger
 from kafka import KafkaProducer
 import random
+import json
 
 
 class KafkaDispatcher():
   def __init__(self, topic, value, key) -> None:
     self.topic = topic
-    self.value = value
     self.key = key
-    self.producer = KafkaProducer(
-      bootstrap_servers=['localhost:9092'],
-      key_serializer=str.encode,
-      value_serializer=str.encode
-    )
+    self.value = value
+    if isinstance(value, str):
+      self.producer = KafkaProducer(
+        bootstrap_servers=['localhost:9092'],
+        key_serializer=str.encode,
+        value_serializer=str.encode
+      )
+    if isinstance(value, dict):
+      self.producer = KafkaProducer(
+        bootstrap_servers=['localhost:9092'],
+        key_serializer=str.encode,
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+      )
   
   def send(self):
     logger = CustomLogger().get_logger()
@@ -21,8 +29,18 @@ class KafkaDispatcher():
       future_record = self.producer.send(self.topic, value=self.value, key=self.key).get(timeout=60)
       logger.info(f"SUCCESS! Order sent to {future_record.topic} | PARTITION {future_record.partition}")
 
+
 if __name__ == '__main__':
   order = f"Order = {str(random.randint(0, 50))} : {str(random.randint(0, 50))}"
+  user_id = str(random.choice(list(range(1, 101))))
+  dispatcher = KafkaDispatcher("ECOMMERCE_NEW_ORDER", value=order, key=user_id) 
+  dispatcher.send()
+
+  order = {
+    "title": "day one",
+    "order_number": str(random.choice(list(range(1, 101))))
+  }
+
   user_id = str(random.choice(list(range(1, 101))))
   dispatcher = KafkaDispatcher("ECOMMERCE_NEW_ORDER", value=order, key=user_id) 
   dispatcher.send()
